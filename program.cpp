06e5992392b1632d7d889d60a7d4b69d67473e68
Kahoneki <shinyzorua6@gmail.com>
2023-10-12 13:22:57 +0100

    Added depth buffer

--- program.cpp
+++ program.cpp
@@ -43,6 +43,8 @@ int main() {
 	}
 
 	glViewport(0, 0, winX, winY);
+	glEnable(GL_DEPTH_TEST);
+
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	
@@ -170,7 +172,7 @@ int main() {
 	glm::mat4 view = glm::mat4(1.0f);
 	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 	glm::mat4 projection;
-	projection = glm::perspective(PI / 4, aspectRatio, 0.1f, 100.0f);
+	projection = glm::perspective(PI / 2, aspectRatio, 0.1f, 100.0f);
 	
 
 
@@ -182,7 +184,7 @@ int main() {
 
 		//Background colour
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, textures[0]);
@@ -214,7 +216,7 @@ int main() {
 		ourShader.setFloat("mixPercentage", mixPercentage);
 
 		//Rotate the cube
-		model = glm::rotate(model, (float)glfwGetTime() * 0.005f, glm::vec3(0.5f, 1.0f, 0.0f));
+		model = glm::rotate(model, (float)(sin(glfwGetTime()) * 0.005), glm::vec3(0.5f, 1.0f, 0.0f));
 
 		ourShader.setMat4("model", model);
 		ourShader.setMat4("view", view);
